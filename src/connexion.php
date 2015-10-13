e2f641470e01e5a6ddce51361657767f6604ba90
sullivanvillella <sullivanvillella@MacBook-Pro-de-Sullivan.local>
2015-10-13 10:40:32 +0200

    Bug via Webhost

--- src/connexion.php
+++ src/connexion.php
@@ -1,7 +1,7 @@
-<?php
+<?php 
 	function connect(){
 		try{
-			$db= new PDO('mysql:host=localhost;dbname=bddSioServices;charset=UTF8','root','root');
+			$db= new PDO('mysql:host=31.170.160.91;dbname=a1567653_lgm;charset=UTF8','a1567653_lgm','btsinfo62');
 		}
 		catch(Exception $e){
 			echo 'Echec:' .$e->getMessage();
